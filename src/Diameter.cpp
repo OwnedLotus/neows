ab4b93731a528c1763751151a6b22f88707aa7c9
Jonah Ringdahl <JonahRingdahl@outlook.com>
2024-08-25 22:19:49 -0400

    start impl of diameter init

--- src/Diameter.cpp
+++ src/Diameter.cpp
@@ -3,7 +3,20 @@
 
 using json = nlohmann::json;
 
-Diameter::Diameter(json diameter_json) {}
+Diameter::Diameter(json diameter_json) {
+  auto kilometers = diameter_json["kilometers"];
+  this->kilometer_min_max = std::tuple(kilometers["estimated_diameter_min"],
+                                       kilometers["estimated_diameter_max"]);
+  auto meters = diameter_json["meters"];
+  this->meter_min_max = std::tuple(meters["estimated_diameter_min"],
+                                   meters["estimated_diameter_max"]);
+  auto miles = diameter_json["miles"];
+  this->miles_min_max = std::tuple(miles["estimated_diameter_min"],
+                                   miles["estimated_diameter_max"]);
+  auto feet = diameter_json["feet"];
+  this->feet_min_max = std::tuple(feet["estimated_diameter_min"],
+                                  feet["estimated_diameter_max"]);
+}
 
 void Diameter::DisplayDiameter() {}
 
