865c2e3afffe5c2e1eb8362c2aa5e7826d4091f9
Jonah Ringdahl <jonahringdahl@outlook.com>
2024-09-08 11:49:52 -0400

    add accessors and settors to diameter

--- src/Diameter.hpp
+++ src/Diameter.hpp
@@ -13,6 +13,24 @@ public:
   Diameter &operator=(const Diameter &) = default;
   ~Diameter();
 
+  std::tuple<double, double> GetKilo() { return kilometer_min_max; }
+  std::tuple<double, double> GetMeter() { return meter_min_max; }
+  std::tuple<double, double> GetMiles() { return miles_min_max; }
+  std::tuple<double, double> GetFeet() { return feet_min_max; }
+
+  void SetKilo(double min, double max) {
+    this->kilometer_min_max = std::make_tuple(min, max);
+  }
+  void SetMeter(double min, double max) {
+    this->meter_min_max = std::make_tuple(min, max);
+  }
+  void SetMiles(double min, double max) {
+    this->miles_min_max = std::make_tuple(min, max);
+  }
+  void SetFeet(double min, double max) {
+    this->feet_min_max = std::make_tuple(min, max);
+  }
+
   void DisplayDiameter();
 
 private:
