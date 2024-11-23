8b70765aa75b573f91ecd6cbd5cbc4c983ed06e4
Jonah Ringdahl <JonahRingdahl@outlook.com>
2024-11-23 17:38:48 -0500

    add getters

--- src/CloseApproach.hpp
+++ src/CloseApproach.hpp
@@ -8,6 +8,20 @@ public:
   CloseApproach(json close_approach_json);
   ~CloseApproach();
 
+  std::string GetApproachDate() { return close_approach_date; }
+  std::string GetApproachDateFull() { return close_approach_date_full; }
+  unsigned long long GetEpochDate() { return epoch_date_close_approach; }
+  std::string GetRelativeKPS() { return relative_vel_kps; }
+  std::string GetRelativeKPH() { return relative_vel_kph; }
+  std::string getRelativeMPH() { return relative_vel_mph; }
+
+  std::string GetMissAstro() { return miss_dist_astronomical; }
+  std::string GetMissLunar() { return miss_dist_lunar; }
+  std::string GetMissKilo() { return miss_dist_kilometer; }
+  std::string GetMissMiles() { return miss_dist_miles; }
+  std::string GetOrbitBody() { return orbiting_body; }
+
+
   void PrintCloseApproach();
 
 private:
