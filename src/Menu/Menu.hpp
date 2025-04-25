3d791778258051b0e11a1f87636e530340c1b9aa
Jonah Ringdahl <JonahRingdahl@outlook.com>
2025-04-25 11:59:06 -0400

    Implement UI

--- src/Menu/Menu.hpp
+++ src/Menu/Menu.hpp
@@ -1,3 +1,13 @@
-class Menu {
+#include "MenuItem.hpp"
+#include <vector>
+#include <string>
 
+class Menu {
+	Menu();
+	~Menu();
+
+	std::vector<MenuItem> uiItems;
+	void UpdateMenu();
+
+	std::string GetKey();
 };
