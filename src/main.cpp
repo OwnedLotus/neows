22d2be1b7b5ea647895cd83e13e8028cdf49de6a
Jonah Ringdahl <JonahRingdahl@outlook.com>
2025-04-30 22:38:49 -0400

    reformat main

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,19 @@
 #include "Neo/NeosCurrier.hpp"
 #include "utils/raygui.h"
 
-#include <cmath>
-#include <cstdlib>
 #include <fstream>
 #include <raylib.h>
 
-
-#include <nlohmann/json.hpp>
 #include <httplib.h>
+#include <nlohmann/json.hpp>
 #include <string>
 
-void get_api_keys(std::string& k, std::string& v);
+void get_api_keys(std::string &k, std::string &v);
 
 int main(void) {
   std::string base_url = "https://api.nasa.gov";
-  std::string path = "/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=";
+  std::string path =
+      "/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=";
 
   std::string key;
   std::string value;
@@ -29,26 +27,26 @@ int main(void) {
   Camera3D camera = {0};
   camera.position = (Vector3){20.0f, 20.0f, 20.0f}; // Camera position
   camera.target = (Vector3){0.0f, 0.0f, 0.0f};      // Camera looking at point
-  camera.up = (Vector3){0.0f, 1.0f, 0.0f}; // Camera up vector (rotation towards target)
+  camera.up =
+      (Vector3){0.0f, 1.0f, 0.0f}; // Camera up vector (rotation towards target)
   camera.fovy = 45.0f;             // Camera field-of-view Y
   camera.projection = CAMERA_PERSPECTIVE;
   Vector3 earthPosition = {0.0, 0.0, 0.0};
   float startTime = GetTime();
-  
+
   // Some bug with loading the asteroid
   Model asteroidModel = LoadModel("assets/Asteroid.glb");
 
   auto currier = new NeosCurrier(true, &asteroidModel);
 
-
-  #ifdef DEBUG
-    path += "DEMO_KEY";
-  #endif // DEBUG
+#ifdef DEBUG
+  path += "DEMO_KEY";
+#endif // DEBUG
   path += value;
 
   // Hanging loading of program
-  //currier->ReachAPI(base_url, path);
-  
+  // currier->ReachAPI(base_url, path);
+
   DisableCursor();
 
   SetTargetFPS(60);
@@ -69,9 +67,9 @@ int main(void) {
     currier->DrawNeos();
     currier->DrawSelectedNeoPointer();
 
-    DrawLine3D((Vector3) {0,0,0}, (Vector3) {20,0,0}, RED);
-    DrawLine3D((Vector3) {0,0,0}, (Vector3) {0,20,0}, GREEN);
-    DrawLine3D((Vector3) {0,0,0}, (Vector3) {0,0,20}, BLUE);
+    DrawLine3D((Vector3){0, 0, 0}, (Vector3){20, 0, 0}, RED);
+    DrawLine3D((Vector3){0, 0, 0}, (Vector3){0, 20, 0}, GREEN);
+    DrawLine3D((Vector3){0, 0, 0}, (Vector3){0, 0, 20}, BLUE);
 
     DrawGrid(20, 1.0f);
 
@@ -83,7 +81,7 @@ int main(void) {
   }
 
   CloseWindow();
-  //UnloadModel(earthModel);
+  // UnloadModel(earthModel);
   UnloadModel(asteroidModel);
 
   delete currier;
@@ -91,7 +89,7 @@ int main(void) {
   return 0;
 }
 
-void get_api_keys(std::string& k, std::string& v) {
+void get_api_keys(std::string &k, std::string &v) {
   std::string outputstr;
   std::ifstream env_file(".env");
   if (env_file.is_open()) {
