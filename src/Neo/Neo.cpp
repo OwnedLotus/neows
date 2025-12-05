65a29144b6c9e687459492ff1c26aa5aeab6f4ac
Jonah Ringdahl <JonahRingdahl@outlook.com>
2025-12-05 11:53:18 -0500

    remove unnecessary logic from neos

--- src/Neo/Neo.cpp
+++ src/Neo/Neo.cpp
@@ -2,191 +2,87 @@
 #include "Diameter.hpp"
 #include "raylib.h"
 
-#include <fstream>
-#include <iostream>
+#include <cstdint>
+#include <limits>
 #include <memory>
 #include <nlohmann/json.hpp>
+#include <nlohmann/json_fwd.hpp>
 #include <string>
 #include <vector>
 
-Neo::Neo(std::shared_ptr<Model> neo_model, std::string _id,
-         std::string _neo_id) {
-  this->model = neo_model;
-  id = _id;
-  neo_ref_id = _neo_id;
+void Neo::SetLink(nlohmann::json &link_json) {
+  this->link = link_json.get<std::string>();
 }
 
-Neo::~Neo() {
-#ifdef DEBUG
-  std::cout << "Deleted Neo: " << this->GetID() << '\n';
-#endif
-  this->diameter.reset();
-  for (auto approach : this->close_approach) {
-    approach.reset();
+void Neo::SetID(nlohmann::json &id_json) {
+  using integer = nlohmann::json::number_integer_t;
+  auto ivalue = id_json.get<integer>();
+  if (!(ivalue < std::numeric_limits<int>::min() ||
+        ivalue > std::numeric_limits<int>::max())) {
+    this->id = id_json.get<int64_t>();
+  }
+
+  throw std::runtime_error("ID is out of range");
+}
+
+void Neo::SetNeoID(nlohmann::json &neo_id_json) {
+  this->neo_ref_id = neo_id_json.get<std::string>();
+}
+
+void Neo::SetName(nlohmann::json &name_json) {
+  this->name = name_json.get<std::string>();
+}
+
+void Neo::SetNasaJPLURL(nlohmann::json &nasa_jps_url_json) {
+  this->nasa_jpl_url = nasa_jps_url_json.get<std::string>();
+}
+
+void Neo::SetMagnitude(nlohmann::json &magnitude_json) {
+  this->absolute_magnitude_h = magnitude_json.get<float>();
+}
+
+void Neo::SetDiameter(nlohmann::json &diameter_json) {
+  if (diameter_json.is_null()) {
+    this->diameter = std::make_unique<Diameter>(Diameter(diameter_json));
   }
 }
 
-void Neo::SetName(std::string n) { this->name = n; }
-void Neo::SetNeoID(std::string id) { this->neo_ref_id = id; }
-void Neo::SetID(std::string id) { this->id = id; }
-void Neo::SetLimitedName(std::string n) { this->name_lim = n; }
-void Neo::SetDesignation(std::string d) { this->designation = d; }
-void Neo::SetLink(std::string l) { this->link = l; }
-void Neo::SetMagnitude(float m) { this->absolute_magnitude_h = m; }
-void Neo::SetHazardous(bool h) { this->is_hazardous = h; }
+void Neo::SetHazardous(nlohmann::json &hazardous_json) {
+  this->is_potentially_hazardous_asteroid = hazardous_json.get<bool>();
+}
+
+void Neo::SetCloseApproach(std::vector<nlohmann::json> &close_approach_json) {
+  for (const auto &ca : close_approach_json) {
+    this->close_approach.push_back(
+        std::make_unique<CloseApproach>(CloseApproach(ca)));
+  }
+}
+
+void Neo::SetIsSentryObject(nlohmann::json &is_sentry_object_json) {
+  this->is_sentry_oject = is_sentry_object_json.get<bool>();
+}
+
 void Neo::SetRenderPosition(Vector3 position) { this->position = position; }
 void Neo::SetRenderRadius(float r) { this->render_radius = r; }
-void Neo::SetDate(std::string date) { this->date = date; }
-void Neo::SetIsSentryObject(bool is_sentry) {
-  this->is_sentry_oject = is_sentry;
-}
 
-void Neo::SetDiameter(nlohmann::json diameter_json) {
-  if (this->diameter == nullptr) {
-    this->diameter = std::make_unique<Diameter>(Diameter(diameter_json));
-    return;
-  }
-
-  this->diameter.reset();
-  this->diameter = std::make_unique<Diameter>(Diameter(diameter_json));
-}
-
-void Neo::SetCloseApproach(nlohmann::json close_approach_json) {
-  for (nlohmann::json::iterator it = close_approach_json.begin();
-       it != close_approach_json.end(); it++) {
-    this->close_approach.emplace_back(
-        std::make_unique<CloseApproach>(CloseApproach(*it)));
-  }
-}
-
-std::string Neo::GetID() { return id; }
-std::string Neo::GetNeoID() { return neo_ref_id; }
-std::string Neo::GetName() { return name; }
-std::string Neo::GetLimitedName() { return name_lim; }
-std::string Neo::GetDesignation() { return designation; }
-std::string Neo::GetLink() { return link; }
+int64_t Neo::GetID() { return id; }
+const std::string &Neo::GetNeoID() { return neo_ref_id; }
+const std::string &Neo::GetName() { return name; }
+const std::string &Neo::GetLink() { return link; }
 float Neo::GetMagnitude() { return absolute_magnitude_h; }
-bool Neo::GetHazardous() { return is_hazardous; }
-std::unique_ptr<Diameter> &Neo::GetDiameter() { return this->diameter; }
-float Neo::GetRenderRadius() { return this->render_radius; }
-Vector3 Neo::GetRenderPosition() { return this->position; }
+bool Neo::GetHazardous() { return is_potentially_hazardous_asteroid; }
+const std::unique_ptr<const Diameter> &Neo::GetDiameter() {
+  return this->diameter;
+}
 bool Neo::GetIsSentryObject() { return this->is_sentry_oject; }
-std::string Neo::GetDate() { return this->date; }
-std::vector<std::unique_ptr<CloseApproach>> &Neo::GetCloseApproach() {
+
+float Neo::GetRenderRadius() { return this->render_radius; }
+const Vector3 &Neo::GetRenderPosition() { return this->position; }
+
+
+const std::vector<std::unique_ptr<const CloseApproach>> &Neo::GetCloseApproach() {
   return this->close_approach;
 }
-
-// implement httplib get query when I have obtained the key from
-std::vector<std::unique_ptr<Neo>> &
-Neo::GetNeos(std::vector<std::unique_ptr<Neo>> &neos) {
-  return neos;
-}
-
-std::vector<std::unique_ptr<Neo>> &
-Neo::GetNeosDebugOffline(std::vector<std::unique_ptr<Neo>> &neos,
-                         std::shared_ptr<Model> neo_model) {
-  std::cout << "Start load Sample Data" << '\n';
-  std::ifstream f("data/sample.json");
-  nlohmann::json data = nlohmann::json::parse(f);
-  return InjestJsonDataOffline(data, neos, neo_model);
-}
-
-void Neo::DisplayNeo() {
-  std::cout << "Id: " << this->GetID() << '\n';
-  std::cout << "Referenced Id: " << this->GetNeoID() << '\n';
-  std::cout << "Name: " << this->GetName() << '\n';
-  std::cout << "Limited Name: " << this->GetLimitedName() << '\n';
-  std::cout << "Link: " << this->GetLink() << '\n';
-  this->diameter->DisplayDiameter();
-
-  for (auto n : this->close_approach) {
-    n->PrintCloseApproach();
-  }
-
-  std::cout << "Sentry Object: " << (this->is_sentry_oject ? "True" : "False")
-            << '\n';
-
-  std::cout << '\n';
-}
-
-std::vector<std::unique_ptr<Neo>> &
-Neo::InjestJsonDataOffline(nlohmann::json data,
-                           std::vector<std::unique_ptr<Neo>> &neos,
-                           std::shared_ptr<Model> neo_model) {
-  std::cout << ("Start Injest Json Data offline") << '\n';
-  auto links = data["links"];
-  auto pages = data["page"];
-  auto neos_data = data["near_earth_objects"];
-
-  int json_size = pages["size"];
-  std::cout << "Size: " << json_size << '\n';
-  int elements = pages["total_elements"];
-  std::cout << "Total Elements: " << elements << '\n';
-  int total_pages = pages["total_pages"];
-  std::cout << "Total Pages: " << total_pages << '\n';
-
-  for (int i = 0; i < json_size; i++) {
-    nlohmann::json neo_data = neos_data[i];
-    auto n = std::make_unique<Neo>(neo_model, neo_data["id"],
-                                   neo_data["neo_reference_id"]);
-
-    n->date = "";
-    n->SetName(neo_data["name"]);
-    n->SetLimitedName(neo_data["name_limited"]);
-    n->SetDesignation(neo_data["designation"]);
-    n->SetLink(neo_data["nasa_jpl_url"]);
-
-    n->SetDiameter(neo_data["estimated_diameter"]);
-    n->SetCloseApproach(neo_data["close_approach_data"]);
-
-    n->SetIsSentryObject(neo_data["is_sentry_object"]);
-
-    n->SetRenderRadius(1);
-
-    neos.emplace_back(std::move(n));
-  }
-
-  return neos;
-}
-
-std::vector<std::shared_ptr<Neo>> &
-InjestJsonData(nlohmann::json data, std::vector<std::shared_ptr<Neo>> &neos,
-               std::shared_ptr<Model> neo_model) {
-  auto links = data["links"];
-  auto pages = data["page"];
-  auto neos_data = data["near_earth_objects"];
-
-  int json_size = pages["size"];
-  std::cout << "Size: " << json_size << '\n';
-  int elements = pages["total_elements"];
-  std::cout << "Total Elements: " << elements << '\n';
-  int total_pages = pages["total_pages"];
-  std::cout << "Total Pages: " << total_pages << '\n';
-
-  for (int i = 0; i < json_size; i++) {
-    nlohmann::json neo_data = neos_data[i];
-    auto n = std::make_unique<Neo>(
-        Neo(neo_model, neo_data["id"], neo_data["neo_reference_id"]));
-
-    n->SetName(neo_data["name"]);
-    n->SetLimitedName(neo_data["name_limited"]);
-    n->SetDesignation(neo_data["designation"]);
-    n->SetLink(neo_data["nasa_jpl_url"]);
-
-    n->SetDiameter(neo_data["estimated_diameter"]);
-    n->SetCloseApproach(neo_data["close_approach_data"]);
-
-    n->SetIsSentryObject(neo_data["is_sentry_object"]);
-
-    // default radius
-    n->SetRenderRadius(1);
-
-    neos.emplace_back(std::move(n));
-  }
-
-  return neos;
-}
-
 // TODO!
 // the render radius should be based on the diameter that is recieved from
 // the query from the api
--- src/Neo/Neo.hpp
+++ src/Neo/Neo.hpp
@@ -10,78 +10,50 @@
 
 class Neo {
 public:
-  Neo(std::shared_ptr<Model> neo_model, std::string _id, std::string _neo_id);
-
-  static std::vector<std::unique_ptr<Neo>> &
-  GetNeos(std::vector<std::unique_ptr<Neo>> &neos);
-
-  static std::vector<std::unique_ptr<Neo>> &
-  GetNeosDebug(std::vector<std::unique_ptr<Neo>> &neos);
-
-  static std::vector<std::unique_ptr<Neo>> &
-  GetNeosDebugOffline(std::vector<std::unique_ptr<Neo>> &neos,
-                      std::shared_ptr<Model> neo_model);
-
-  static std::vector<std::unique_ptr<Neo>> &
-  InjestJsonDataOffline(nlohmann::json data,
-                        std::vector<std::unique_ptr<Neo>> &neos,
-                        std::shared_ptr<Model> neo_model);
-
-  static std::vector<std::unique_ptr<Neo>> &
-  InjestJsonData(nlohmann::json data, std::vector<std::unique_ptr<Neo>> &neos,
-                 std::shared_ptr<Model> neo_model);
-
+  Neo() = default;
   // getters setters //
 
-  void SetName(std::string n);
-  void SetNeoID(std::string id);
-  void SetID(std::string id);
-  void SetLimitedName(std::string n);
-  void SetDesignation(std::string d);
-  void SetLink(std::string l);
-  void SetMagnitude(float m);
-  void SetDiameter();
-  void SetHazardous(bool h);
-  void SetDiameter(nlohmann::json diameter_json);
-  void SetCloseApproach(nlohmann::json close_approach_json);
+  void SetLink(nlohmann::json &link_json);
+  void SetID(nlohmann::json &id_json);
+  void SetNeoID(nlohmann::json &neo_id_json);
+  void SetName(nlohmann::json &name_json);
+  void SetNasaJPLURL(nlohmann::json &nasa_jpl_url_json);
+  void SetMagnitude(nlohmann::json &magnitude_json);
+  void SetDiameter(nlohmann::json &diameter_json);
+  void SetHazardous(nlohmann::json &hazardous_json);
+  void SetCloseApproach(std::vector<nlohmann::json> &close_approach_json);
+  void SetIsSentryObject(nlohmann::json &is_sentry_object_json);
+
   void SetRenderPosition(Vector3 position);
   void SetRenderRadius(float r);
-  void SetIsSentryObject(bool is_sentry);
-  void SetDate(std::string date);
 
-  std::string GetID();
-  std::string GetNeoID();
-  std::string GetName();
-  std::string GetLimitedName();
-  std::string GetDesignation();
-  std::string GetLink();
+  int64_t GetID();
+  const std::string &GetNeoID();
+  const std::string &GetName();
+  const std::string &GetLimitedName();
+  const std::string &GetLink();
   float GetMagnitude();
   bool GetHazardous();
-  std::unique_ptr<Diameter> &GetDiameter();
-  Vector3 GetRenderPosition();
+  const std::unique_ptr<const Diameter> &GetDiameter();
+  const Vector3 &GetRenderPosition();
   float GetRenderRadius();
   bool GetIsSentryObject();
-  std::string GetDate();
-  std::vector<std::unique_ptr<CloseApproach>> &GetCloseApproach();
+  const std::vector<std::unique_ptr<const CloseApproach>> &GetCloseApproach();
 
-  void DisplayNeo();
   void Draw();
 
 private:
   Vector3 position;
   float render_radius;
 
-  std::string date;
-  std::string id;
+  std::string link;
+  int64_t id;
   std::string neo_ref_id;
   std::string name;
-  std::string name_lim;
-  std::string designation;
-  std::string link;
+  std::string nasa_jpl_url;
   float absolute_magnitude_h;
-  bool is_hazardous;
-  std::unique_ptr<Diameter> diameter;
-  std::vector<std::unique_ptr<CloseApproach>> close_approach;
+  std::unique_ptr<const Diameter> diameter;
+  bool is_potentially_hazardous_asteroid;
+  std::vector<std::unique_ptr<const CloseApproach>> close_approach;
   bool is_sentry_oject;
-  std::shared_ptr<Model> model;
 };
